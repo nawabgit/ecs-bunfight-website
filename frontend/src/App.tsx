da03e091e456f83dbb0df87985bbca1e4827934f
Nawab <nawab.mir14@gmail.com>
2020-09-14 23:09:45 +0100

    added basic content background to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,38 @@
 import React, { useState, useEffect, useMemo } from "react";
 import styled from "styled-components";
 
-const TableHeader = styled.div`
-  font-size: "12pt";
-  width: "300px";
+const AppContainer = styled.div`
+  display: flex;
+  min-height: 100vh;
+  max-height: 100vh;
+  box-sizing: border-box;
+
+  background-image: radial-gradient(
+      circle at 16% 83%,
+      rgba(148, 148, 148, 0.06) 0%,
+      rgba(148, 148, 148, 0.06) 50%,
+      rgba(63, 63, 63, 0.06) 50%,
+      rgba(63, 63, 63, 0.06) 100%
+    ),
+    radial-gradient(
+      circle at 68% 87%,
+      rgba(66, 66, 66, 0.06) 0%,
+      rgba(66, 66, 66, 0.06) 50%,
+      rgba(105, 105, 105, 0.06) 50%,
+      rgba(105, 105, 105, 0.06) 100%
+    ),
+    radial-gradient(
+      circle at 38% 50%,
+      rgba(123, 123, 123, 0.06) 0%,
+      rgba(123, 123, 123, 0.06) 50%,
+      rgba(172, 172, 172, 0.06) 50%,
+      rgba(172, 172, 172, 0.06) 100%
+    ),
+    linear-gradient(90deg, hsl(18, 0%, 1%), hsl(18, 0%, 1%));
 `;
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  return (
-    <>
-      <button onClick={() => setCounter(counter + 1)}>Increment</button>
-      <div>{counter}</div>
-    </>
-  );
+  return <AppContainer></AppContainer>;
 }
 
 export default App;
