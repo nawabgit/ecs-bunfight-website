8dda7127b8fc83fe37a4da9f68c94677e0274b90
Nawab <nawab.mir14@gmail.com>
2020-09-14 23:09:30 +0100

    added global style to remove css defaults

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+
+  html, body {
+    margin: 0;
+    padding: 0;
+    background: #F7F8FB;
+    font-size: 14pt;
+    font-family: Roboto, sans-serif;
+  }
+
+  * {
+    scrollbar-width: thin;
+    scrollbar-color: gray gray;
+  }
+  
+  *::-webkit-scrollbar {
+    width: 4px;
+  }
+  *::-webkit-scrollbar-thumb {
+    background-color: gray;
+    border-radius: 20px;
+  }
+`;
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
