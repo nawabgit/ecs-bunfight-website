e7999a2f4069ff0fea94fd7a5da7bf26745ef31c
Nawab <nawab.mir14@gmail.com>
2020-09-29 04:53:09 +0100

    added db_index to User model

--- backend/questions/core/models.py
+++ backend/questions/core/models.py
@@ -3,8 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    username = models.CharField(('username'), max_length=30, unique=True,
-        error_messages={
-            'unique': ("A user with that username already exists."),
-        },
+    username = models.CharField(('username'), max_length=30, unique=True, db_index=True,
         primary_key=True)
