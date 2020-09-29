1276ef644490224bd817515ecb48dfd21a947344
Akhilesh Shastri <as14g18@soton.ac.uk>
2020-09-29 06:39:35 +0100

    Revert models

--- backend/questions/core/models.py
+++ backend/questions/core/models.py
@@ -3,5 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    username = models.CharField(('username'), max_length=30, unique=True, db_index=True,
-        primary_key=True)
+    pass
